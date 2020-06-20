55fa8bbcb31d81a6b5fd9fa6320691726050bfb9
chi-moua <kongfuechimoua@gmail.com>
2020-06-19 23:46:34 -0500

    Adding initial pydantic models.

---
+++ app/router/model/model.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel
+
+from app.database.model.enum import ExpenseType
+
+
+class ExpenseCreate(BaseModel):
+    '''Represents a pydantic model that handles the create expense request.'''
+    date: str
+    amount: float
+    business: str
+    expense_type: ExpenseType
+    description: str = None
+
+class Expense(ExpenseCreate):
+    '''Model that handles the expense request.'''
+    expense_id = int
+
+
+class TripCreate(BaseModel):
+    '''Represents a pydantic model that handles the trip request.'''
+    name: str
+    start_date: str
+    end_date: str
+    description: str
+
+
+class Trip(TripCreate):
+    '''Model that handles the trip request.'''
+    trip_id = int
+
+
+class TravelExpenseCreate(BaseModel):
+    '''Represents a pydantic model that handles the travel expense request.'''
+    expense_id = int
+    trip_id = int
+    
+
+class TravelExpense(TravelExpenseCreate):
+    '''Model that handles the travel expense request.'''
+    travel_expense_id: int = None
+
+
+
